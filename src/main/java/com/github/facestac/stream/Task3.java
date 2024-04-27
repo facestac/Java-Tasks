0956a6133846efe6b778cdc5ad041cdc174c1c3b
facestac <facestac@student.21-school.ru>
2024-04-27 22:47:20 +0500

    stream Task3 completed

---
+++ src/main/java/com/github/facestac/stream/Task3.java
@@ -0,0 +1,43 @@
+package com.github.facestac.stream;
+
+
+/*
+Jaden Smith, the son of Will Smith, is the star of films such as
+The Karate Kid (2010) and After Earth (2013). Jaden is also known
+for some of his philosophy that he delivers via Twitter.
+When writing on Twitter, he is known for almost always capitalizing
+every word. For simplicity, you'll have to capitalize each word,
+check out how contractions are expected to be in the example below.
+
+Your task is to convert strings to how they would be written by Jaden Smith.
+The strings are actual quotes from Jaden Smith, but they are not capitalized
+in the same way he originally typed them.
+
+Example:
+
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+Note that the Java version expects a return value of null for an empty string or null.
+ */
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+
+public class Task3 {
+    public static void main(String[] args) {
+        Task3 t = new Task3();
+        String exampleString = "How can mirrors be real if our eyes aren't real";
+        System.out.println(t.convertJadenCasedString(exampleString));
+    }
+
+    public String convertJadenCasedString(String str) {
+//        if (str.isEmpty()) return null;
+
+        return Arrays.stream(str.split(" "))
+                .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1))
+                .collect(Collectors.joining(" "));
+    }
+
+
+}
